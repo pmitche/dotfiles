3a0c3dcdf2679a11d2ba9e731e8fd055a0519bc4
Paul Philip Mitchell <paulpmit@gmail.com>
2023-09-27 23:13:43 +0200

    install homebrew if not already installed

--- brew.sh
+++ brew.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
+# Check if Homebrew is installed
+if ! command -v brew &> /dev/null; then
+    # Homebrew is not installed, so install it
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
